d9a5bf7a8f5927bf0a01a7ae71d27547932bbdd6
edf1003 <edf1003@alu.ubu.es>
2023-05-19 13:51:09 +0200

    Corrección de k-means en clustering

--- src/app/pages/clustering/clustering-methods/kmeans/kmeans.component.ts
+++ src/app/pages/clustering/clustering-methods/kmeans/kmeans.component.ts
@@ -12,6 +12,7 @@ import { ColorsToSend } from 'src/app/services/colors.service';
 })
 export class KmeansComponent implements OnInit {
   @Output() labels: number[] = [];
+  centroids: number[] = [];
   distanceForm: FormGroup;
   numberOfClusters: number = 0;
   distancesEucl: number[][] = [];
@@ -74,8 +75,9 @@ export class KmeansComponent implements OnInit {
   kMeans(distances: number[][], k: number) {
     const maxIterations = 100;
     const n = distances.length;
-    var assignments = new Array(n).fill(0);
-    var centroids = this.initializeCentroids(distances, k);
+    const d = distances[0].length;
+    let assignments = new Array(n).fill(0);
+    let centroids = this.initializeCentroids(distances, k);
 
     let converged = false;
     let iteration = 0;
@@ -95,7 +97,6 @@ export class KmeansComponent implements OnInit {
       centroids = this.updateCentroids(distances, assignments, k);
       iteration++;
     }
-
     this.labels = assignments;
   }
 
@@ -108,7 +109,7 @@ export class KmeansComponent implements OnInit {
       let closestCentroid = -1;
 
       for (let j = 0; j < centroids.length; j++) {
-        const dist = distances[i][j];
+        const dist = this.calculateDistance(distances[i], centroids[j]);
 
         if (dist < minDist) {
           minDist = dist;
@@ -124,6 +125,7 @@ export class KmeansComponent implements OnInit {
 
   initializeCentroids(distances: number[][], k: number): number[][] {
     const n = distances.length;
+    const d = distances[0].length;
     const centroids = new Array(k);
     const assignedPoints = new Set();
 
@@ -135,8 +137,9 @@ export class KmeansComponent implements OnInit {
         if (!assignedPoints.has(j)) {
           let minDist = Infinity;
 
-          for (let l = 0; l < centroids.length; l++) {
-            const dist = distances[j][l];
+          for (let l = 0; l < n; l++) {
+            if (assignedPoints.has(l)) continue;
+            const dist = this.calculateDistance(distances[j], distances[l]);
 
             if (dist < minDist) {
               minDist = dist;
@@ -151,7 +154,7 @@ export class KmeansComponent implements OnInit {
       }
 
       assignedPoints.add(farthestPoint);
-      centroids[i] = distances[farthestPoint];
+      centroids[i] = distances[farthestPoint].slice();
     }
 
     return centroids;
@@ -184,13 +187,24 @@ export class KmeansComponent implements OnInit {
       }
 
       for (let m = 0; m < d; m++) {
-        centroid[m] /= counts[i];
+        centroid[m] /= counts[i] || 1;
       }
 
       centroids[i] = centroid;
     }
 
     return centroids;
+  }
+
+  calculateDistance(point1: number[], point2: number[]): number {
+    const d = point1.length;
+    let sum = 0;
+
+    for (let i = 0; i < d; i++) {
+      sum += Math.pow(point1[i] - point2[i], 2);
+    }
+
+    return Math.sqrt(sum);
   }
 
   getColor(index: number): string {
--- src/app/services/resumePdf.service.ts
+++ src/app/services/resumePdf.service.ts
@@ -9,39 +9,34 @@ export class ResumePdf {
   convertHTMLtoPDF(elementId: string, filename: string): void {
     const captureHTML = async (): Promise<string | null> => {
       const element = document.getElementById(elementId);
+      if (!element) return null;
 
-      if (element) {
-        const canvas = await html2canvas(element);
-        return canvas.toDataURL('image/png');
-      }
-
-      return null;
+      const canvas = await html2canvas(element);
+      return canvas.toDataURL('image/png');
     };
 
     const createPDF = async (imageBase64: string): Promise<ArrayBuffer> => {
       const image = new Image();
       image.src = imageBase64;
 
-      return new Promise((resolve) => {
-        image.onload = () => {
-          const aspectRatio = image.width / image.height;
-          const width = image.width;
-          const height = image.height;
-
-          const margin = 20;
-          const pageWidth = width + margin * 2;
-          const pageHeight = height + margin * 2;
-
-          const doc = new jsPDF({
-            orientation: width > height ? 'landscape' : 'portrait',
-            unit: 'px',
-            format: [pageWidth, pageHeight],
-          });
-
-          doc.addImage(image, 'PNG', margin, margin, width, height);
-          resolve(doc.output('arraybuffer'));
-        };
+      await new Promise((resolve) => {
+        image.onload = resolve;
       });
+
+      const { width, height } = image;
+      const aspectRatio = width / height;
+      const margin = 20;
+      const pageWidth = width + margin * 2;
+      const pageHeight = height + margin * 2;
+
+      const doc = new jsPDF({
+        orientation: width > height ? 'landscape' : 'portrait',
+        unit: 'px',
+        format: [pageWidth, pageHeight],
+      });
+
+      doc.addImage(image, 'PNG', margin, margin, width, height);
+      return doc.output('arraybuffer');
     };
 
     const downloadPDF = (pdfBytes: ArrayBuffer): void => {
@@ -58,11 +53,10 @@ export class ResumePdf {
 
     const convertAndDownloadPDF = async (): Promise<void> => {
       const imageBase64 = await captureHTML();
+      if (!imageBase64) return;
 
-      if (imageBase64) {
-        const pdfBytes = await createPDF(imageBase64);
-        downloadPDF(pdfBytes);
-      }
+      const pdfBytes = await createPDF(imageBase64);
+      downloadPDF(pdfBytes);
     };
 
     convertAndDownloadPDF();
